9b03b5d8bd4b26314f6f65371c11d3143abfa74e
Finn Stokes <finn.stokes@gmail.com>
2020-06-22 14:57:07 +0200

    Refactored units module after meeting on 2020-06-22.
    Added units.a method providing unit value for lattice units.
    Moved scale setting out of Scale and in to Value.
    Added handwritten comparison methods for Values.
    Added method to convert value to specific unit.

--- tests/test_units.py
+++ tests/test_units.py
@@ -11,14 +11,11 @@ def test_mismatched():
     with pytest.raises(ValueError):
         two_a = a + a2
 
-    latt.set_scale(a, 0.0904 * units.fm)
-    latt2.set_scale(a2, 0.0911 * units.fm)
-
     with pytest.raises(ValueError):
         two_a = a + a2
 
     with pytest.raises(ValueError):
-        two_a = a / a.in_units(units.Physical())
+        two_a = a / a.set_scale(a, 0.0904 * units.fm)
 
 
 def test_convert():
@@ -28,14 +25,16 @@ def test_convert():
     a = units.Value(1.0, units.Length, latt)
     a2 = units.Value(1.0, units.Length, latt2)
 
-    latt.set_scale(a, 0.0904 * units.fm)
-    latt2.set_scale(a2, 0.0911 * units.fm)
+    a_phys = a.set_scale(a, 0.0904 * units.fm)
+    with pytest.raises(ValueError):
+        a2_phys = a2.set_scale(a, 0.0904 * units.fm)
+    a2_phys = a2.set_scale(a2, 0.0911 * units.fm)
 
-    assert a.in_units(units.Physical()) == 0.0904 * units.fm
+    assert a_phys == 0.0904 * units.fm
 
-    two_a = a.in_units(units.Physical()) + a2.in_units(units.Physical())
+    two_a = a_phys + a2_phys
 
     assert two_a == (0.0904 + 0.0911) * units.fm
 
-    m = 0.48 / a
-    assert (m.in_units(units.Physical()) / units.GeV).value == pytest.approx(1.04775386973)
+    m = 0.48 / a_phys
+    assert m.in_unit(units.GeV) == pytest.approx(1.04775386973)
