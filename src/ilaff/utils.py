c43f52314a56ccafb23970070d5368b80f017077
Ryan Bignell <ryan07bignell@gmail.com>
2020-07-06 18:50:01 +0930

    fixed further mypy error

--- src/ilaff/utils.py
+++ src/ilaff/utils.py
@@ -2,20 +2,21 @@ from ilaff.units.quantity import Quantity
 from ilaff.measurements.measurementJack import measurementJack
 
 
-import numpy as np #type:ignore
+import numpy as np  # type:ignore
 
 def effE( G: Quantity, deltaT: int, a: Quantity) -> Quantity:
     """
     Effective Mass Function
     """
-    return ( (1.0/(deltaT*a) ) * np.log( G.value/np.roll(G.value,deltaT) ) )
+    EffE: Quantity = ( (1.0/(deltaT*a) ) * np.log( G.value/np.roll(G.value,deltaT) ) )
+    return EffE
 
 def effEMJ( GMJ: measurementJack, deltaT: int, a: Quantity) -> measurementJack:
     """
     Effective energy function for measurementJack correlators
     """
     print('TODO: Actually do for jackknifes properly')
-    #eh. This seems to work
+    # eh. This seems to work
     return measurementJack(
         GMJ.iValue,
         effE( GMJ.dValue, deltaT, a ),
